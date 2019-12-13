cf210fde0311228bf54fa30600ff6a2fe7e2b942
flatulentbadger <seanmmckenzie@outlook.com>
2019-12-12 20:53:50 -0800

    add javascript numbers, strings, arrays, objects

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+//Numbers
+//numbers and be computed with +, -, *, /, %, ++ increment, --decrement
+//NaN means Not a Number
+var pizza = 5;
+var soda = 1;
+var bread = 3;
+
+var total = pizza + soda;
+
+console.log(total);
+
+/**
+ * The console. log() is afunction in Javascript which is used to print any kind of variables defined in it or to just print any message that needs to be displayed to the user.
+ */
+
+ //String
+
+ const name = 'Slippery Badger'
+
+ const welcome = "Welcome to the party, Pal"
+
+
+
+ console.log(name);
+ console.log(welcome);
+
+ //Array = a list
+ var fruits = ['apples', 'grapes', 'oranges', 'bananas'];
+ var rockBand = ['Pantera', 'Tool', 'Nirvana', 'Radiohead'];
+
+ console.log(fruits);
+ console.log(rockBand);
+
+ //Object
+
+ var car = {
+     type: "Fiat",
+     model: "500",
+     color: "white",
+ };
+
+ console.log(car);
+
+ // Boolean
+ // everything with a real value is true 0, null, undefined is false
+
+
